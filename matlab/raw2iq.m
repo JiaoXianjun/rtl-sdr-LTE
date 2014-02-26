404975939e36abc702cdf83555f2f63f09b455d7
Jiao Xianjun(Ryan) <putaoshu@msn.com>
2014-02-26 15:41:51 +0800

    have IQ sample divided by 128 to conform to C code of LTE-Cell-Scanner

--- matlab/raw2iq.m
+++ matlab/raw2iq.m
@@ -6,5 +6,10 @@
 function b = raw2iq(a)
 c = a(1:2:end,:) + 1i.*a(2:2:end,:);
 % c = a(2:2:end,:) + 1i.*a(1:2:end,:); % swap
+
 % b = c- kron(ones(size(c,1),1), ( sum(c, 1)./size(c,1) ));
-b = c - 128 - 1i.*128;
+
+% b = c - 128 - 1i.*128;
+
+b = (c - 128 - 1i.*128)./128; % to comform to C code of LTE-Cell-Scanner
+
