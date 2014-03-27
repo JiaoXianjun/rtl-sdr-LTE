32988b587486bd47df5ee5076f4147ec3ff9e9a6
Jiao Xianjun <putaoshu@gmail.com>
2014-03-27 22:11:36 +0800

    change -100:5:100 to -60:5:55 to align with C

--- matlab/CellSearch.m
+++ matlab/CellSearch.m
@@ -77,7 +77,8 @@ THRESH2_N_SIGMA = 3;
 
 [~, td_pss] = pss_gen;
 
-f_search_set = -100e3:5e3:100e3; % frequency offset searching range
+% f_search_set = -100e3:5e3:100e3; % frequency offset searching range
+f_search_set = -60e3:5e3:55e3; % frequency offset searching range; aligned to C
 pss_fo_set = pss_fo_set_gen(td_pss, f_search_set);
 
 if use_file_flag == 0
